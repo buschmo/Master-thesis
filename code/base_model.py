88f41eb5f9ab9edaaa2c44d594037d5df62bda5b
Moritz Busch <mail@moritz-busch.de>
2022-11-09 10:36:56 +0100

    Updated formatting

--- code/base_model.py
+++ code/base_model.py
@@ -2,8 +2,9 @@ import torch
 from torch import nn
 from pathlib import Path
 
+
 class BaseModel(nn.Module):
-    
+
     def __init__(self, filepath=None, filename=None):
         super().__init__()
         if filepath:
@@ -11,16 +12,16 @@ class BaseModel(nn.Module):
         else:
             self.filename = filename if filename else self.__str__()
             self.update_filepath()
-        
+
     def __repr__(self):
         raise NotImplementedError
-    
+
     def __str__(self):
         raise NotImplementedError
-        
+
     def update_filepath(self):
         dir_name = Path("checkpoints").absolute()
-        self.filepath = Path(dir_name, self.__str__(), self.filename +".pt")
+        self.filepath = Path(dir_name, self.__str__(), self.filename + ".pt")
 
     def save(self):
         if not self.filepath.parent.exists():
@@ -32,15 +33,15 @@ class BaseModel(nn.Module):
     def save_checkpoint(self, epoch_num):
         if not self.filepath.parent.exists():
             self.filepath.parent.mkdir(parents=True)
-            
-        filename = self.filepath.with_stem(f"{self.filename}_{epoch_num}")
-        torch.save(self.state_dict(), filename)
+
+        torch.save(self.state_dict(), self.filepath.with_stem(
+            f"{self.filepath.stem}_{epoch_num}"))
         print(f"Model checkpoint {self} saved for epoch {epoch_num}.")
-    
+
     def load(self):
         self.load_state_dict(torch.load(self.filepath))
 
-    def getCheckpoints(self) -> dict[str,list[int]]:
+    def getCheckpoints(self) -> dict[str, list[int]]:
         d = {}
         for file in self.filepath.parent.iterdir():
             if not "_" in file.stem:
@@ -48,6 +49,6 @@ class BaseModel(nn.Module):
             name = file.stem.split("_")[0]
             number = file.stem.split("_")[-1]
             d.setdefault(name, []).append(number)
-        for k,v in d.items():
+        for k, v in d.items():
             d[k] = sorted(v)
         return d
