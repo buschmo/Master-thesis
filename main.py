87f571ce4371e3b808db12c4818766fa5c137b6b
Moritz Busch <mail@moritz-busch.de>
2022-11-14 01:41:17 +0100

    Updated main.py for new datasets

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import torch
 
 from code.naive_model import NaiveVAE
-from utils.dataset import SimpleGermanDataset
+from utils.dataset import SimpleGermanDataset, SimpleWikipediaDataset
 from utils.trainer import Trainer
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -14,13 +14,14 @@ def main():
     else:
         print("Cuda was found.")
 
+    # dataset = SimpleWikipediaDataset()
     dataset = SimpleGermanDataset()
     model = NaiveVAE(input_size=dataset.getInputSize(), filename=dataset.__str__())
     trainer = Trainer(dataset, model, checkpoint_index=50)
 
     trainer.train_model(
         batch_size=64,
-        num_epochs=1000
+        num_epochs=500
     )
 
 
