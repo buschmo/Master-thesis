abbc025abd56ab903ce3dcd257897eedcdccd702
Moritz Busch <mail@moritz-busch.de>
2022-11-18 10:45:10 +0100

    Adapted main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import torch
+import datetime
+import time
 
 from code.naive_model import NaiveVAE
 from utils.dataset import SimpleGermanDataset, SimpleWikipediaDataset
@@ -15,14 +17,21 @@ def main():
         print("Cuda was found.")
 
     # dataset = SimpleWikipediaDataset()
-    dataset = SimpleGermanDataset()
-    model = NaiveVAE(input_size=dataset.getInputSize(), foldername=dataset.__str__())
-    trainer = Trainer(dataset, model, checkpoint_index=50)
+    # dataset = SimpleGermanDataset()
+    for Dataset in [SimpleGermanDataset, SimpleWikipediaDataset]:
+        dataset = Dataset()
+        for num_epochs in [100,1000]:
+            ts = time.time()
+            timestamp = datetime.datetime.fromtimestamp(ts).strftime(
+                '%Y-%m-%d_%H:%M:%S'
+            )
+            model = NaiveVAE(input_size=dataset.getInputSize(), foldername=dataset.__str__(), timestamp=timestamp)
+            trainer = Trainer(dataset, model, checkpoint_index=50, timestamp=timestamp)
 
-    trainer.train_model(
-        batch_size=64,
-        num_epochs=500
-    )
+            trainer.train_model(
+                batch_size=64,
+                num_epochs=num_epochs
+            )
 
 
 if __name__ == "__main__":
