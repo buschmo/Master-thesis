5491d9c1ccad236b35c79da7bc37b55f1201b132
Moritz Busch <mail@moritz-busch.de>
2022-12-02 00:20:18 +0100

    Updated main function

--- main.py
+++ main.py
@@ -20,18 +20,19 @@ def main():
     # dataset = SimpleGermanDataset()
     for Dataset in [SimpleGermanDataset, SimpleWikipediaDataset]:
         dataset = Dataset()
-        for num_epochs in [100,1000]:
-            ts = time.time()
-            timestamp = datetime.datetime.fromtimestamp(ts).strftime(
-                '%Y-%m-%d_%H:%M:%S'
-            )
-            model = NaiveVAE(input_size=dataset.getInputSize(), foldername=dataset.__str__(), timestamp=timestamp)
-            trainer = Trainer(dataset, model, checkpoint_index=50, timestamp=timestamp)
+        for num_epochs in [1000]:
+            for use_reg_loss in [True, False]:
+                ts = time.time()
+                timestamp = datetime.datetime.fromtimestamp(ts).strftime(
+                    '%Y-%m-%d_%H:%M:%S'
+                )
+                model = NaiveVAE(input_size=dataset.getInputSize(), foldername=dataset.__str__(), timestamp=timestamp)
+                trainer = Trainer(dataset, model, checkpoint_index=0, use_reg_loss=use_reg_loss,timestamp=timestamp)
 
-            trainer.train_model(
-                batch_size=64,
-                num_epochs=num_epochs
-            )
+                trainer.train_model(
+                    batch_size=64,
+                    num_epochs=num_epochs
+                )
 
 
 if __name__ == "__main__":
