67799f59603c43ad6b222d1b70492f274295d6e2
Moritz Busch <mail@moritz-busch.de>
2023-04-04 14:11:11 +0200

    Renamed variable

--- models/tvae_trainer.py
+++ models/tvae_trainer.py
@@ -95,24 +95,24 @@ class TVAETrainer(Trainer):
         # compute and add regularization loss if needed
         if self.use_reg_loss:
             reg_loss = 0.0
-            reg = 0.0
+            reg_unscaled = 0.0
             if type(self.reg_dim) == tuple:
                 # remove last sentence token
                 z_tilde = z_tilde[:, :-1, :]
                 # [batch, sequence, d_model] -> [batch * sequence, d_model]
                 z_tilde = z_tilde.reshape(-1, z_tilde.shape[-1])
                 for dim in self.reg_dim:
-                    reg_loss_temp, reg_temp = self.compute_reg_loss(
+                    reg_loss_temp, reg_unscaled_temp = self.compute_reg_loss(
                         z_tilde, labels[:, dim], dim, gamma=self.gamma, factor=self.delta)
                     reg_loss += reg_loss_temp
-                    reg += reg_temp
+                    reg_unscaled += reg_unscaled_temp
             else:
                 raise TypeError(
                     "Regularization dimension must be a tuple of integers")
             loss += reg_loss
         else:
             reg_loss = torch.Tensor([0])
-            reg = torch.Tensor([0])
+            reg_unscaled = torch.Tensor([0])
 
         # compute accuracy
         accuracy = self.mean_accuracy(
@@ -126,7 +126,7 @@ class TVAETrainer(Trainer):
             "KLD": dist_loss,
             "KLD_unscaled": kld,
             "regularization": reg_loss,
-            "regularization_unscaled": reg
+            "regularization_unscaled": reg_unscaled
         }
 
         return loss_dict, accuracy
